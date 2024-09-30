12a25ad967eb2f1af6752abf9e7b5fbf8b60192e
ArshChauhan1101 <achauhan4830@conestogac.on.ca>
2024-09-30 01:06:37 -0400

    Half done

--- React-to-do/src/App.jsx
+++ React-to-do/src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+
 function App() {
+  
+
+  const [todos, setTodos] = useState([
+    'Go to the gym',
+    'Eat more fruites',
+    'Pick Kids from school'
+  ])
+
+  function handleAddTodos(newTodo)
+  {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
   return (
     <>
-    <div>{4 * 4}</div>
+      <TodoInput handleAddTodos = {handleAddTodos}/>
+      <TodoList todos = {todos}/>
     </>
   )
 }
